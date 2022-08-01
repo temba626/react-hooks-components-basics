25715773922d75b29304fa4f161c2fe834ea4979
Brandon Temba <brandontemba560@gmail.com>
2022-08-01 11:15:18 +0300

    commit tree 15ebf7a8859ce0390a523dffd11009d8d3527fd2

--- src/Comment.js
+++ src/Comment.js
@@ -5,3 +5,15 @@ function Comment() {
 }
 
 export default Comment;
+
+/*Similary one can use the following code to replicate teh above:
+
+
+class Comment extends React.Component {
+  render() {
+    return <div>Naturally, I agree with this article.</div>;
+  }
+}
+
+
+*/
